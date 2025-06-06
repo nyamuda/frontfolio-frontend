c6dee068ccb98c184f3c5e2545eb15eb2fbb8d66
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-06 02:54:06 +0200

    reset password route added

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,8 @@ import RegisterUser from "@/components/auth/RegisterUser.vue";
 import EmailVerificationRequest from "@/components/auth/EmailVerificationRequest.vue";
 import VerifyEmailOtp from "@/components/auth/VerifyEmailOtp.vue";
 import PasswordResetRequest from "@/components/auth/PasswordResetRequest.vue";
+import VerifyPasswordResetOtp from "@/components/auth/VerifyPasswordResetOtp.vue";
+import ResetPassword from "@/components/auth/ResetPassword.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,18 +52,23 @@ const router = createRouter({
         },
         {
           path: "email-verification/request",
-          name: "Email Verification Request",
           component: EmailVerificationRequest,
         },
         {
           path: "email-verification/verify",
-          name: "Verify Email",
           component: VerifyEmailOtp,
         },
         {
           path: "password-reset/request",
-          name: "Password Reset Request",
           component: PasswordResetRequest,
+        },
+        {
+          path: "password-reset/verify-otp",
+          component: VerifyPasswordResetOtp,
+        },
+        {
+          path: "password-reset/reset",
+          component: ResetPassword,
         },
       ],
     },
