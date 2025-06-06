6305708d46c659fb39aa61ef4cf6f194708d3bc7
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-06 05:14:08 +0200

    title section component added

---
+++ src/components/shared/TitleSection.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="alignItemsClass + ' d-flex flex-column'">
+    <p class="heading-line rounded"></p>
+    <p class="fs-4 text-capitalize mb-0 text-secondary">{{ subtitle }}</p>
+    <h2 :class="classObject">{{ title }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  subtitle: {
+    type: String,
+  },
+  alignItems: {
+    type: String,
+    default: "start",
+  },
+  titleSize: {
+    type: String,
+    default: "medium",
+  },
+});
+
+const classObject = computed(() => {
+  return {
+    "h2 fw-bold mb-4 text-secondary": props.titleSize == "small",
+    "h1 fw-bold mb-4 text-secondary": props.titleSize == "normal" || props.titleSize == "medium",
+    "display-5 fw-bold mb-4 text-secondary": props.titleSize == "large",
+  };
+});
+
+//Class to use to align the items
+const alignItemsClass = computed(() => {
+  if (props.alignItems == "center") {
+    return "align-items-center";
+  } else if (props.alignItems == "end") {
+    return "align-items-end";
+  } else {
+    return "align-items-start";
+  }
+});
+</script>
