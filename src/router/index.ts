1119e49b8e5d188499ef86bbeb1b31a34f088e73
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-06 02:44:54 +0200

    login route registered

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import { useAuthStore } from "@/stores/auth";
 import AuthView from "@/views/AuthView.vue";
 import UserAccount from "@/components/auth/UserAccount.vue";
+import LoginUser from "@/components/auth/LoginUser.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
             }
             return true;
           },
+        },
+        {
+          path: "login",
+          name: "Login",
+          component: LoginUser,
         },
       ],
     },
