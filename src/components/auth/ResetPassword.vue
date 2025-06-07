3de72de3af117aa7f77ca386ba5a523f049d73db
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-07 08:11:02 +0200

    password reset token check if expired added

--- src/components/auth/ResetPassword.vue
+++ src/components/auth/ResetPassword.vue
@@ -1,40 +1,21 @@
 <template>
   <div class="">
     <!--If the reset link has expired-->
-    <div
-      v-if="hasTokenExpiredOrIsInvalid"
-      class="d-flex justify-content-center align-items-center"
-    >
+    <div v-if="hasTokenExpiredOrIsInvalid" class="d-flex justify-content-center align-items-center">
       <div class="text-center">
         <i class="fas fa-exclamation-triangle text-danger fa-4x"></i>
         <h4 class="mt-2 text-danger">Reset Link Expired</h4>
-        <p class="text-muted">
-          The password reset link has expired or is invalid.
-        </p>
+        <p class="text-muted">The password reset link has expired or is invalid.</p>
         <p class="text-muted">Please request a new password reset link.</p>
         <router-link to="/email/password-reset-request">
-          <Button
-            severity="primary"
-            label=" Request new link"
-            icon="fas fa-sync-alt"
-          />
+          <Button severity="primary" label=" Request new link" icon="fas fa-sync-alt" />
         </router-link>
       </div>
     </div>
     <!--Password reset form-->
-    <form
-      v-else
-      class="reset-password-form m-auto"
-      @submit.prevent="submitForm"
-    >
-      <TitleSection
-        title="Set Your New Password"
-        title-size="small"
-        align-items="start"
-      />
-      <p class="mb-3">
-        Please enter your new password and confirm it to proceed.
-      </p>
+    <form v-else class="reset-password-form m-auto" @submit.prevent="submitForm">
+      <TitleSection title="Set Your New Password" title-size="small" align-items="start" />
+      <p class="mb-3">Please enter your new password and confirm it to proceed.</p>
       <!-- Password input -->
       <div class="form-group mb-3">
         <FloatLabel variant="on">
@@ -50,12 +31,7 @@
           </IconField>
           <label for="resetPassword">Password</label>
         </FloatLabel>
-        <Message
-          size="small"
-          severity="error"
-          v-if="v$.password.$error"
-          variant="simple"
-        >
+        <Message size="small" severity="error" v-if="v$.password.$error" variant="simple">
           <div v-for="error of v$.password.$errors" :key="error.$uid">
             <div>{{ error.$message }}</div>
           </div>
@@ -77,12 +53,7 @@
           </IconField>
           <label for="resetPasswordConfirm">Confirm Password</label>
         </FloatLabel>
-        <Message
-          size="small"
-          severity="error"
-          v-if="v$.passwordConfirm.$error"
-          variant="simple"
-        >
+        <Message size="small" severity="error" v-if="v$.passwordConfirm.$error" variant="simple">
           <div v-for="error of v$.passwordConfirm.$errors" :key="error.$uid">
             <div>{{ error.$message }}</div>
           </div>
@@ -96,9 +67,7 @@
         size="small"
         type="submit"
         severity="primary"
-        :label="
-          isResettingPassword ? 'Resetting password...' : 'Submit new password'
-        "
+        :label="isResettingPassword ? 'Resetting password...' : 'Submit new password'"
         :loading="isResettingPassword"
         :disabled="v$.$errors.length > 0 || isResettingPassword"
       />
@@ -106,45 +75,43 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { computed, onMounted, ref } from "vue";
-import { useStore } from "vuex";
+import { useAuthStore } from "@/stores/auth";
 import { useRouter } from "vue-router";
 //Vuelidate for login form validation
 import { useVuelidate } from "@vuelidate/core";
 import { required, helpers, sameAs } from "@vuelidate/validators";
 import { Message } from "primevue";
-import { isJwtExpired } from "jwt-check-expiration";
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
 import Button from "primevue/button";
 import IconField from "primevue/iconfield";
 import InputIcon from "primevue/inputicon";
 import { useToast } from "primevue/usetoast";
-import TitleSection from "../Common/Elements/TitleSection.vue";
+import TitleSection from "../shared/TitleSection.vue";
+import { jwtDecode } from "jwt-decode";
 
 const router = useRouter();
 const toast = useToast();
 // Access the store
-const store = useStore();
+const authStore = useAuthStore();
 
 //provided token from the URL
-let providedToken = ref("");
+const providedToken = ref("");
 
 //check if the reset token has expired or not
-let hasTokenExpiredOrIsInvalid = ref(true);
+const hasTokenExpiredOrIsInvalid = ref(true);
 
 //form validation with Vuelidate start
 const resetPasswordForm = ref({
   password: "",
   passwordConfirm: "",
 });
-const passwordRule = helpers.regex(
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/
-);
-let passwordErrorMessage =
+const passwordRule = helpers.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/);
+const passwordErrorMessage =
   "Password must be at least 8 characters long and contain a mix of letters, numbers, and special characters";
-let passwordNotMatching = "The passwords you entered do not match";
+const passwordNotMatching = "The passwords you entered do not match";
 const rules = computed(() => ({
   password: {
     required,
@@ -154,7 +121,7 @@ const rules = computed(() => ({
     required,
     sameAsPassword: helpers.withMessage(
       passwordNotMatching,
-      sameAs(computed(() => resetPasswordForm.value.password))
+      sameAs(computed(() => resetPasswordForm.value.password)),
     ),
   },
 }));
@@ -168,11 +135,14 @@ onMounted(() => {
 
     //get the token provided in the URL from
     //when the user clicks the reset button in their confirmation email
-    providedToken.value = router.currentRoute.value.query.token ?? "";
+    providedToken.value = router.currentRoute.value.query.token?.toString() ?? "";
 
     //check if the token has expired or not
-    hasTokenExpiredOrIsInvalid.value = isJwtExpired(providedToken.value);
+    //check if access token has expired or not
+    const exp = jwtDecode(providedToken.value).exp; // in seconds
+    const now = Math.floor(Date.now() / 1000); // current time in seconds
 
+    hasTokenExpiredOrIsInvalid.value = exp ? exp > now : true;
     //throw an exception if the token has expired
     if (hasTokenExpiredOrIsInvalid.value) throw new Error();
   } catch {
@@ -180,9 +150,9 @@ onMounted(() => {
   }
 });
 
-let isResettingPassword = ref(false);
+const isResettingPassword = ref(false);
 
-let submitForm = async () => {
+const submitForm = async () => {
   const isFormCorrect = await v$._value.$validate();
   if (isFormCorrect && providedToken.value) {
     isResettingPassword.value = true;
