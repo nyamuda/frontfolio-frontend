c076224620325ee00e29624cc895e17ee18d7f83
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-10 17:14:21 +0200

    features section created

---
+++ src/components/home/FeaturesSection.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="py-6">
+    <div class="container">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 order-md-1 text-center text-md-start">
+            <img
+              class="img-fluid mb-4"
+              src="assets/img/illustrations/ultimate-feature.png"
+              alt=""
+            />
+          </div>
+          <div class="col-md-6 text-center text-md-start">
+            <h6 class="fw-bold fs-4 display-3 lh-sm">Ultimate features<br />that we built</h6>
+            <p class="my-4 pe-xl-5">
+              The rise of mobile devices transforms the way we consume information entirely.
+            </p>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="mb-4">
+                  <div class="py-4">
+                    <img
+                      class="img-fluid"
+                      src="assets/img/illustrations/app.png"
+                      width="90"
+                      alt=""
+                    />
+                  </div>
+                  <h5 class="fw-bold text-undefined">App Development</h5>
+                  <p class="mt-2 mb-0">
+                    Get your blood tests delivered at home collect a sample from the news your blood
+                    tests.
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-4">
+                  <div class="py-4">
+                    <img
+                      class="img-fluid"
+                      src="assets/img/illustrations/ux.png"
+                      width="90"
+                      alt=""
+                    />
+                  </div>
+                  <h5 class="fw-bold text-undefined">UX Planning</h5>
+                  <p class="mt-2 mb-0">
+                    Get your blood tests delivered at home collect a sample from the news your blood
+                    tests.
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-4">
+                  <div class="py-4">
+                    <img
+                      class="img-fluid"
+                      src="assets/img/illustrations/cloud.png"
+                      width="90"
+                      alt=""
+                    />
+                  </div>
+                  <h5 class="fw-bold text-undefined">Cloud Storage</h5>
+                  <p class="mt-2 mb-0">
+                    Get your blood tests delivered at home collect a sample from the news your blood
+                    tests.
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-4">
+                  <div class="py-4">
+                    <img
+                      class="img-fluid"
+                      src="assets/img/illustrations/support.png"
+                      width="90"
+                      alt=""
+                    />
+                  </div>
+                  <h5 class="fw-bold text-undefined">Customer support</h5>
+                  <p class="mt-2 mb-0">
+                    Get your blood tests delivered at home collect a sample from the news your blood
+                    tests.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <a class="btn btn-lg btn-primary rounded-pill hover-top" href="#" role="button"
+              >See all</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end of .container-->
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
   <main>
     <HeroSection />
     <ServicesSection />
+    <FeaturesSection />
     <HowItWorksSection />
   </main>
 </template>
 
 <script setup lang="ts">
+import FeaturesSection from "@/components/home/FeaturesSection.vue";
 import HeroSection from "@/components/home/HeroSection.vue";
 import HowItWorksSection from "@/components/home/HowItWorksSection.vue";
 import ServicesSection from "@/components/home/ServicesSection.vue";
