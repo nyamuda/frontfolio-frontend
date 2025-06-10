04d8dce895b846fe5345a2fe70939eba7df9d5c3
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-10 10:37:25 +0200

    services section home component created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-  <main></main>
+  <main>
+    <HeroSection />
+    <ServicesSection />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeroSection from "@/components/home/HeroSection.vue";
+import ServicesSection from "@/components/home/ServicesSection.vue";
+</script>
