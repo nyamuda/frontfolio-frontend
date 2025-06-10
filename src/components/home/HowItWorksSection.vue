7e2892a15500eacc9fa8a464937ec2b8aeb6f902
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-10 16:20:20 +0200

    added how it works homepage section

---
+++ src/components/home/HowItWorksSection.vue
@@ -0,0 +1,151 @@
+<template>
+  <section class="py-0">
+    <div
+      class="bg-holder"
+      style="
+        background-image: url(assets/img/illustrations/how-it-works.png);
+        background-position: center bottom;
+        background-size: cover;
+      "
+    ></div>
+    <!--/.bg-holder-->
+
+    <div class="container-lg">
+      <div class="row justify-content-center">
+        <div class="col-sm-8 col-md-9 col-xl-5 text-center pt-8">
+          <h5 class="fw-bold fs-3 fs-xxl-5 lh-sm mb-3 text-white">How it works</h5>
+          <p class="mb-5 text-white">
+            Take your pick from the supply chain and participate in agribusiness projects that are
+            backed up not only by Zou, but also by the best land, family heritage, innovation and
+            overall superior expertise.
+          </p>
+        </div>
+        <div class="col-sm-9 col-md-12 col-xxl-9">
+          <div class="theme-tab">
+            <ul class="nav justify-content-between">
+              <li class="nav-item" role="presentation">
+                <a
+                  class="nav-link active fw-semi-bold"
+                  href="#bootstrap-tab1"
+                  data-bs-toggle="tab"
+                  data-bs-target="#tab1"
+                  id="tab-1"
+                  ><span class="nav-item-circle-parent"
+                    ><span class="nav-item-circle">01</span></span
+                  ></a
+                >
+              </li>
+              <li class="nav-item" role="presentation">
+                <a
+                  class="nav-link fw-semi-bold"
+                  href="#bootstrap-tab2"
+                  data-bs-toggle="tab"
+                  data-bs-target="#tab2"
+                  id="tab-2"
+                  ><span class="nav-item-circle-parent"
+                    ><span class="nav-item-circle">02</span></span
+                  ></a
+                >
+              </li>
+              <li class="nav-item" role="presentation">
+                <a
+                  class="nav-link fw-semi-bold"
+                  href="#bootstrap-tab3"
+                  data-bs-toggle="tab"
+                  data-bs-target="#tab3"
+                  id="tab-3"
+                  ><span class="nav-item-circle-parent"
+                    ><span class="nav-item-circle">03</span></span
+                  ></a
+                >
+              </li>
+              <li class="nav-item" role="presentation">
+                <a
+                  class="nav-link fw-semi-bold"
+                  href="#bootstrap-tab4"
+                  data-bs-toggle="tab"
+                  data-bs-target="#tab4"
+                  id="tab-4"
+                  ><span class="nav-item-circle-parent"
+                    ><span class="nav-item-circle">04</span></span
+                  ></a
+                >
+              </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+              <div
+                class="tab-pane fade show active"
+                id="tab1"
+                role="tabpanel"
+                aria-labelledby="tab-1"
+              >
+                <div class="row align-items-center my-6 mx-auto">
+                  <div class="col-md-6 col-lg-5 offset-md-1">
+                    <h3 class="fw-bold lh-base text-white">
+                      Select your farmshare and complete reservation form here.
+                    </h3>
+                  </div>
+                  <div class="col-md-5 text-white offset-lg-1">
+                    <p class="mb-0">
+                      Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
+                      cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
+                      maxime placeat facere
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab-2">
+                <div class="row align-items-center my-6 mx-auto">
+                  <div class="col-md-6 col-lg-5 offset-md-1">
+                    <h3 class="fw-bold lh-base text-white">
+                      The Farm Share has been one of the best new additions to our life
+                    </h3>
+                  </div>
+                  <div class="col-md-5 text-white offset-lg-1">
+                    <p class="mb-0">
+                      We the farmers choose the vegetables in your share each week. A Farm Share is
+                      perfect for those who love the weekly surprise, and who look forward to
+                      cooking with inspiration from the seasons.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab-3">
+                <div class="row align-items-center my-6 mx-auto">
+                  <div class="col-md-6 col-lg-5 offset-md-1">
+                    <h3 class="fw-bold lh-base text-white">
+                      There is so much love and thought put into this CSA. You can feel that.
+                    </h3>
+                  </div>
+                  <div class="col-md-5 text-white offset-lg-1">
+                    <p class="mb-0">
+                      We the farmers choose the vegetables in your share each week. A Farm Share is
+                      perfect for those who love the weekly surprise, and who look forward to
+                      cooking with inspiration from the seasons.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane fade" id="tab4" role="tabpanel" aria-labelledby="tab-4">
+                <div class="row align-items-center my-6 mx-auto">
+                  <div class="col-md-6 col-lg-5 offset-md-1">
+                    <h3 class="fw-bold lh-base text-white">
+                      The Farm Share has been one of the best new additions to our life
+                    </h3>
+                  </div>
+                  <div class="col-md-5 text-white offset-lg-1">
+                    <p class="mb-0">
+                      We the farmers choose the vegetables in your share each week. A Farm Share is
+                      perfect for those who love the weekly surprise, and who look forward to
+                      cooking with inspiration from the seasons.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/components/home/ServicesSection.vue
+++ src/components/home/ServicesSection.vue
@@ -1,5 +1,189 @@
 <template>
-  <div></div>
+  <div>
+    <!-- Services -->
+    <div id="services" class="cards-1 bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <div class="text-container">
+              <h2>Services that we offer</h2>
+              <p>
+                Greatly hearted has who believe. Drift allow green son walls years for blush. Sir
+                margaret drawings repeated recurred exercise laughing may you
+              </p>
+              <p>
+                Do repeated whatever to welcomed absolute no. Fat surprise although more words
+                outlived
+              </p>
+              <ul class="list-unstyled li-space-lg">
+                <li class="d-flex">
+                  <i class="pi pi-check"></i>
+                  <div class="flex-grow-1">And informed shy dissuade property. Musical by</div>
+                </li>
+                <li class="d-flex">
+                  <i class="fas fa-square"></i>
+                  <div class="flex-grow-1">He drawing savings an. No we stand avoid</div>
+                </li>
+                <li class="d-flex">
+                  <i class="fas fa-square"></i>
+                  <div class="flex-grow-1">Announcing of invita mrore wo tion principle</div>
+                </li>
+              </ul>
+            </div>
+            <!-- end of text-container -->
+          </div>
+          <!-- end of col -->
+          <div class="col-lg-7">
+            <div class="card-container">
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="pi pi-pencil"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Space analysis and planning</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="far fa-clock"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Design and color choosing</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="far fa-comments"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Materials and delivery</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="fas fa-tools"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Execute the concept</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="fas fa-chart-pie"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Creating great atmosphere</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+
+              <!-- Card -->
+              <div class="card">
+                <div class="card-icon">
+                  <span class="far fa-chart-bar"></span>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Evaluation and reporting</h5>
+                </div>
+              </div>
+              <!-- end of card -->
+            </div>
+            <!-- end of container -->
+          </div>
+          <!-- end of col -->
+        </div>
+        <!-- end of row -->
+      </div>
+      <!-- end of container -->
+    </div>
+    <!-- end of cards-1 -->
+    <!-- end of services -->
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+/********************/
+/*     Services     */
+/********************/
+.cards-1 {
+  padding-top: 9.5rem;
+  padding-bottom: 5.5rem;
+}
+
+.cards-1 h2 {
+  margin-bottom: 1.75rem;
+}
+
+.cards-1 .text-container {
+  margin-bottom: 5rem;
+}
+
+.cards-1 .list-unstyled {
+  margin-top: 1.5rem;
+}
+
+.cards-1 .list-unstyled .fas {
+  color: #cc2973;
+  font-size: 0.375rem;
+  line-height: 1.625rem;
+}
+
+.cards-1 .list-unstyled .flex-grow-1 {
+  margin-left: 0.5rem;
+}
+
+.cards-1 .card-container {
+  text-align: center;
+}
+
+.cards-1 .card {
+  display: inline-block;
+  margin-right: 1.5rem;
+  margin-bottom: 3.75rem;
+  margin-left: 1.5rem;
+  width: 150px;
+  border: none;
+  background-color: transparent;
+  text-align: center;
+  vertical-align: top;
+}
+
+.cards-1 .card-icon {
+  width: 90px;
+  height: 90px;
+  margin-right: auto;
+  margin-bottom: 1.5rem;
+  margin-left: auto;
+  border-radius: 50%;
+  background-color: #ffffff;
+  text-align: center;
+}
+
+.cards-1 .card-icon .pi,
+.cards-1 .card-icon .pi {
+  margin-left: 2rem;
+  color: #cc2973;
+  font-size: 3.75rem;
+}
+
+.cards-1 .card-body {
+  padding: 0;
+}
+
+.cards-1 .card-title {
+  font-weight: 600;
+}
+</style>
