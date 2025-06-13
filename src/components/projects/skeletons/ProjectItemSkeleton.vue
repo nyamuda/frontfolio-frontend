0b630344e58609d7e8953b4c587f6bb09ffaf3d3
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-13 09:32:41 +0200

    added comments to the project item skeleton

--- src/components/projects/skeletons/ProjectItemSkeleton.vue
+++ src/components/projects/skeletons/ProjectItemSkeleton.vue
@@ -2,15 +2,19 @@
   <div>
     <div class="card">
       <div class="card-body">
+        <!-- title and tag -->
         <div class="d-flex flex-wrap justify-content-between align-items-center mb-1 gap-1">
           <Skeleton width="10rem" height="1.5rem" borderRadius="4px" />
           <Skeleton width="5rem" height="1.5rem" borderRadius="20px" />
         </div>
+        <!-- short summary -->
         <Skeleton width="100%" height="3rem" class="mb-2" borderRadius="4px" />
+        <!-- button -->
         <div class="mt-3">
           <Skeleton width="6rem" height="2rem" borderRadius="6px" />
         </div>
       </div>
+      <!-- image -->
       <Skeleton width="100%" height="150px" borderRadius="0" />
     </div>
   </div>
