fc6abfdaffc71bae5eb6df2a2639037f52dc62a2
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-13 09:30:30 +0200

    project item skeleton created

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <TitleSection title="Work in Progress" subtitle="Portfolio Projects" />
+
+    <div class="row">
+      <p class="col-md-6">
+        Here’s a quick overview of your portfolio projects. You can view details, update content, or
+        publish drafts anytime.
+      </p>
+    </div>
     <div class="row">
       <div class="col-sm-6 col-lg-4 g-3" v-for="i in 8" :key="i">
         <ProjectItem />
       </div>
     </div>
+    <ProjectItemSkeleton />
   </div>
 </template>
 
 <script setup lang="ts">
 import TitleSection from "../shared/TitleSection.vue";
 import ProjectItem from "./ProjectItem.vue";
+import ProjectItemSkeleton from "./skeletons/ProjectItemSkeleton.vue";
 </script>
---
+++ src/components/projects/skeletons/ProjectItemSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex flex-wrap justify-content-between align-items-center mb-1 gap-1">
+          <Skeleton width="10rem" height="1.5rem" borderRadius="4px" />
+          <Skeleton width="5rem" height="1.5rem" borderRadius="20px" />
+        </div>
+        <Skeleton width="100%" height="3rem" class="mb-2" borderRadius="4px" />
+        <div class="mt-3">
+          <Skeleton width="6rem" height="2rem" borderRadius="6px" />
+        </div>
+      </div>
+      <Skeleton width="100%" height="150px" borderRadius="0" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Skeleton from "primevue/skeleton";
+</script>
