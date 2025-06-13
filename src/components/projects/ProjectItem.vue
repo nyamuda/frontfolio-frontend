270471b71d38c28bb61d539ef2e68229da6aacdc
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-13 06:51:34 +0200

    tag added to project card item

--- src/components/projects/ProjectItem.vue
+++ src/components/projects/ProjectItem.vue
@@ -1,25 +1,11 @@
 <template>
   <div>
-    <div class="card mb-3">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-        class="card-img-top"
-        alt="Wild Landscape"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a wider card with supporting text below as a natural lead-in to additional
-          content. This content is a little bit longer.
-        </p>
-        <p class="card-text">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </p>
-      </div>
-    </div>
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <div class="d-flex justify-content-between">
+          <h5 class="card-title">Card title</h5>
+          <Tag severity="success" value="Published"></Tag>
+        </div>
         <p class="card-text">
           This is a wider card with supporting text below as a natural lead-in to additional
           content. This content is a little bit longer.
@@ -36,3 +22,7 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Tag from "primevue/tag";
+</script>
--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <ProjectItem />
+    <div class="row">
+      <div class="col-sm-6 col-lg-4 g-3" v-for="i in 8" :key="i">
+        <ProjectItem />
+      </div>
+    </div>
   </div>
 </template>
 
