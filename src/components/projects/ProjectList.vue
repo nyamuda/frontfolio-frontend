ad7b47fc289aa99214aea5ee54395287b9306d08
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-13 09:45:09 +0200

    add new project button added to project list

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -4,21 +4,28 @@
 
     <div class="row">
       <p class="col-md-6">
-        Here’s a quick overview of your portfolio projects. You can view details, update content, or
-        publish drafts anytime.
+        Here’s everything you’ve added so far. You can continue refining your drafts, publish new
+        ones, or revisit existing projects to keep your portfolio fresh and up to date
       </p>
+    </div>
+    <div class="d-flex justify-content-end mb-3">
+      <Button
+        icon="pi pi-plus"
+        label="New project"
+        variant="outlined"
+        aria-label="Add new project"
+      />
     </div>
     <div class="row">
       <div class="col-sm-6 col-lg-4 g-3" v-for="i in 8" :key="i">
         <ProjectItem />
       </div>
     </div>
-    <ProjectItemSkeleton />
   </div>
 </template>
 
 <script setup lang="ts">
 import TitleSection from "../shared/TitleSection.vue";
 import ProjectItem from "./ProjectItem.vue";
-import ProjectItemSkeleton from "./skeletons/ProjectItemSkeleton.vue";
+import Button from "primevue/button";
 </script>
