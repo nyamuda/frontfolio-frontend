14354f3e1fdce63c617a15dc01f57e57ad22f804
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-17 10:48:56 +0200

    paragraph type added to paragraph model

--- src/models/paragraph.ts
+++ src/models/paragraph.ts
@@ -1,8 +1,11 @@
+import type { paragraphType } from "@/types/paragraphType";
+
 export class Paragraph {
   public constructor(
     public title: string,
     public imageUrl: string,
     public imageCaption: string,
     public content: string,
+    public paragraphType: paragraphType,
   ) {}
 }
