2f82dec23edc4349ea122d969ae4415a7beb4fb0
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-17 19:16:58 +0200

    add new paragraph button and click event added to paragraph list component

--- src/components/paragraphs/ParagraphList.vue
+++ src/components/paragraphs/ParagraphList.vue
@@ -2,23 +2,35 @@
   <section>
     <div v-for="(paragraph, index) in paragraphs" :key="index">
       <ParagraphSection
-        @update="(val: Paragraph) => handleParagraphUpdate(index, val)"
+        @update="(val: Paragraph) => updateParagraphAtIndex(index, val)"
         :paragraph="paragraph"
+      />
+    </div>
+    <div class="d-flex justify-content-center align-items-center">
+      <Button
+        @click="addNewParagraph"
+        icon="pi pi-plus"
+        severity="contrast"
+        label="New paragraph"
+        size="small"
       />
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import type { Paragraph } from "@/models/paragraph";
+import { Paragraph } from "@/models/paragraph";
 import { ref, type Ref } from "vue";
+import Button from "primevue/button";
 
 const paragraphs: Ref<Paragraph[]> = ref([]);
 
+const addNewParagraph = () => paragraphs.value.push(new Paragraph());
+
 // Update the paragraph at the specified index
 const updateParagraphAtIndex = (targetIndex: number, updatedParagraph: Paragraph) => {
-  paragraphs.value = paragraphs.value.map((paragraph, currentIndex) => {
-    return currentIndex === targetIndex ? updatedParagraph : paragraph;
-  });
+  paragraphs.value = paragraphs.value.map((paragraph, currentIndex) =>
+    currentIndex === targetIndex ? updatedParagraph : paragraph,
+  );
 };
 </script>
