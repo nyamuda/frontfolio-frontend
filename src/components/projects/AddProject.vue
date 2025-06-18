6d657c9059355347359c6d58192c17f20fcbcbaf
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-18 07:10:57 +0200

    add section description to project background

--- src/components/projects/AddProject.vue
+++ src/components/projects/AddProject.vue
@@ -151,10 +151,9 @@
           </div>
         </template>
         <p class="mb-3">
-          Use this section to add in-depth descriptions of your project. You can talk about your
-          goals, how you approached the build, key features, or anything else that helps explain
-          your work. This is optional, but it's a great way to give more context and highlight your
-          thought process, especially for portfolio reviewers or potential clients.
+          Use this section to add in-depth details about your project. Share your goals, why you
+          built it, how you approached the process, and the key features. It's optional, but a
+          wonderful way to show your thinking and help others understand your work and vision.
         </p>
 
         <ParagraphList />
