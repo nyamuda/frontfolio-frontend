1bd4870d68550b56b1f81c960c8fe742e8015262
P.T Nyamuda <ptnrlab@gmail.com>
2025-06-26 05:56:15 +0200

    added validated paragraph variables to paragraph store

--- src/stores/paragraph.ts
+++ src/stores/paragraph.ts
@@ -1,10 +1,15 @@
-import { ref, type Ref } from "vue";
+import { ref} from "vue";
 import { defineStore } from "pinia";
+import type { ValidatedItem } from "@/interfaces/shared/validatedItem";
+import type { Paragraph } from "@/models/paragraph";
 
 
 export const useParagraphStore = defineStore("paragraph", () => {
 
+const validatedParagraphsForAddProject = ref<ValidatedItem<Paragraph>[]>([]);
+const validatedParagraphsForEditProject = ref<ValidatedItem<Paragraph>[]>([]);
 
-return {}
+
+return {validatedParagraphsForAddProject,validatedParagraphsForEditProject}
 
 })
