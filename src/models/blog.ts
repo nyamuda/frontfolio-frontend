2ee8fd595dc61edf964bfab64881842bb5529caa
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-07 05:23:47 +0200

    created blog model

--- src/models/blog.ts
+++ src/models/blog.ts
@@ -1,12 +1,19 @@
+import { BlogStatus } from "@/enums/blogStatus";
+import type { Paragraph } from "./paragraph";
+
 export class Blog {
   public constructor(
-public id: string | number = 0,
-public title:string="",
-public topic:string:"",
-public summary:string:"",
-public imageUrl:string:"",
-
-
-
-) {}
+    public id: string | number = 0,
+    public title: string = "",
+    public topic: string = "",
+    public summary: string = "",
+    public imageUrl: string = "",
+    public status: BlogStatus = BlogStatus.Draft,
+    public content: Paragraph[] = [],
+    public tags: string[] = [],
+    public userId: number = 0,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
+    public publishedAt: Date = new Date(),
+  ) {}
 }
